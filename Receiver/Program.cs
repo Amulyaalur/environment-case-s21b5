2c903f30f5187f7c19166a974998e7cf6cdb8cbf
AjinkyaGhatol <31014537+AjinkyaGhatol@users.noreply.github.com>
2020-09-16 16:25:38 +0530

    reduce complexity of receiver

--- Receiver/Program.cs
+++ Receiver/Program.cs
@@ -1,54 +1,51 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Receiver
 {
     public struct Properties
     {
         public string property_Name;
-
+       
     }
 
-    class Program
+    public class Program
     {
-        static List<Properties> Properties_list = new List<Properties>();
+        public static List<Properties> Properties_list=new List<Properties>();
         static string[] Properties_names;
-
+        
         static string[] SplitLine(string line)
         {
             string[] split = line.Split(',');
             return split;
         }
-        static void GetPropertyNames()
+        public static void GetPropertyNames()
         {
             string line = string.Empty;
             line = Console.ReadLine();
-            Properties_names = SplitLine(line);
-
+            Properties_names=SplitLine(line);
+            
         }
         static void AssignIndexToProperties()
         {
-            for (int i = 0; i < Properties_names.Length; i++)
+            for(int i=0;i<Properties_names.Length;i++)
             {
                 Properties temp;
                 temp.property_Name = Properties_names[i];
                 Properties_list.Add(temp);
             }
-
+            
         }
         static void GetReadingsFromSensorAndAnalyze()
         {
             string line = string.Empty;
             line = Console.ReadLine();
-
+            
             while (!line.Equals(""))
             {
-
+                
                 string[] values;
-                values = SplitLine(line);
+                values= SplitLine(line);
                 AnalyzeTemperature(values);
                 AnalyzeHumidity(values);
                 line = Console.ReadLine();
@@ -70,50 +67,81 @@ namespace Receiver
         static void AnalyzeTemperature(string[] values)
         {
             int index = getIndex("Temperature");
-            if (index >= 0)
+            if(index>=0)
             {
-                if (int.Parse((values[index].Split('C'))[0]) > 40)
-                {
-                    Console.WriteLine("Temperature reached High Error level:" + values[index]);
-                }
-                else if (int.Parse((values[index].Split('C'))[0]) > 37)
-                {
-                    Console.WriteLine("Temperature reached High Warning level:" + values[index]);
-                }
-                else if (int.Parse((values[index].Split('C'))[0]) < 0)
-                {
-                    Console.WriteLine("Temperature reached Low Error level:" + values[index]);
-                }
-                else if (int.Parse((values[index].Split('C'))[0]) < 4)
-                {
-                    Console.WriteLine("Temperature reached Low Warning level:" + values[index]);
-                }
+                AlertTemperatureIfOutOfLimits(values, index);
             }
-
+            else
+            {
+                Console.WriteLine("CSV does not contain Temperature property.");
+            }
+            
         }
+        static void AlertTemperatureIfOutOfLimits(string[] values, int index)
+        {
+            if (int.Parse((values[index].Split('C'))[0]) > 37)
+            {
+                AlertForHighLimitsForTemperature(values, index);
+            }
+            else if (int.Parse((values[index].Split('C'))[0]) < 4)
+            {
+                AlertForLowerLimitsForTemperature(values, index);
+            }
+        }
+        static void AlertForHighLimitsForTemperature(string[] values, int index)
+        {
+            if (int.Parse((values[index].Split('C'))[0]) > 40)
+            {
+                Console.WriteLine("Temperature reached High Error level:" + values[index]);
+            }
+            else
+            {
+                Console.WriteLine("Temperature reached High Warning level:" + values[index]);
+            }
+        }
+        static void AlertForLowerLimitsForTemperature(string[] values, int index)
+        {
+            if (int.Parse((values[index].Split('C'))[0]) < 0)
+            {
+                Console.WriteLine("Temperature reached Low Error level:" + values[index]);
+            }
+            else
+            {
+                Console.WriteLine("Temperature reached Low Warning level:" + values[index]);
+            }
+        }
+        
         static void AnalyzeHumidity(string[] values)
         {
             int index = getIndex("Humidity");
             //Console.WriteLine(index);
-            if (index >= 0)
+            if(index>=0)
             {
-                if (int.Parse((values[index].Split('%'))[0]) > 90)
-                {
-                    Console.WriteLine("Humidity reached Error level:" + values[index]);
-                }
-                else if (int.Parse((values[index].Split('%'))[0]) > 70)
-                {
-                    Console.WriteLine("Humidity reached Warnig level:" + values[index]);
-                }
+                AlertHumidityIfOutOfLimits(values, index);
+            }
+            else
+            {
+                Console.WriteLine("CSV does not contain Temperature property.");
             }
 
 
         }
+        static void AlertHumidityIfOutOfLimits(string[] values, int index)
+        {
+            if (int.Parse((values[index].Split('%'))[0]) > 90)
+            {
+                Console.WriteLine("Humidity reached Error level:" + values[index]);
+            }
+            else if (int.Parse((values[index].Split('%'))[0]) > 70)
+            {
+                Console.WriteLine("Humidity reached Warnig level:" + values[index]);
+            }
+        }
         static void PrintProperties()
         {
-            for (int i = 0; i < Properties_list.Count; i++)
+            for(int i=0;i<Properties_list.Count;i++)
             {
-                Console.Write(Properties_list[i].property_Name + ",");
+                Console.Write(Properties_list[i].property_Name+",");
             }
             Console.WriteLine();
         }
@@ -123,7 +151,7 @@ namespace Receiver
             AssignIndexToProperties();
             //PrintProperties();
             GetReadingsFromSensorAndAnalyze();
-
+            
         }
     }
 }
