2cd81b94f2344b70aacca3161223443498ad9424
Ajinkya Ghatol <ajinkyas.ghatol@gmail.com>
2020-09-17 20:18:11 +0530

    solve unit test problem

--- Receiver.Tests/UnitTest1.cs
+++ Receiver.Tests/UnitTest1.cs
@@ -19,6 +19,8 @@ namespace Receiver.Tests
             {
                 failure = true;
             }
+            catch(System.NullReferenceException)
+            {}
             Assert.True(failure);
         }
         [Fact]
